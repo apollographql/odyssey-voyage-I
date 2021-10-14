574df03a41ec9b415bbf98c23abac9ec55ee045e
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2021-10-14 16:42:23 -0400

    set up gateway with supergraph

---
+++ location-reviews/index.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require('apollo-server');
+const { ApolloGateway } = require('@apollo/gateway');
+const { readFileSync } = require('fs');
+
+const supergraphSdl = readFileSync('./supergraph-schema.graphql').toString();
+
+const gateway = new ApolloGateway({
+  supergraphSdl,
+});
+
+const server = new ApolloServer({
+  gateway,
+});
+
+server
+  .listen()
+  .then(({ url }) => {
+    console.log(`🚀 Gateway ready at ${url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
