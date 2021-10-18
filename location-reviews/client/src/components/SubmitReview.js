f75558959b1f822523d87fc127904e7d9dc73958
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2021-10-18 10:43:39 -0400

    update cache using refetch queries after submitting a review

--- location-reviews/client/src/components/SubmitReview.js
+++ location-reviews/client/src/components/SubmitReview.js
@@ -1,6 +1,7 @@
 import { gql, useMutation } from '@apollo/client';
 import React, { useState } from 'react';
 import { Input, Select, Button } from '@chakra-ui/react';
+import { GET_LOCATION_DETAILS } from '../pages/Location';
 
 export const SUBMIT_REVIEW = gql`
   mutation submitReview($review: ReviewInput) {
@@ -25,14 +26,17 @@ export default function SubmitReview({ locationId }) {
   const handleChange = (event) => setComment(event.target.value);
 
   const [submitReview] = useMutation(SUBMIT_REVIEW, {
-    variables: { review: { comment, rating } },
+    variables: { review: { comment, rating: parseInt(rating, 10), locationId: locationId } },
+    refetchQueries: [
+      { query: GET_LOCATION_DETAILS, variables: { locationId: locationId } }, // DocumentNode object parsed with gql
+      'getLocationDetails', // Query name
+    ],
   });
 
   return (
     <div>
       <Input placeholder="Write your review here" size="lg" value={comment} onChange={handleChange} />
       <Select
-        placeholder="Rate the location"
         value={rating}
         onChange={(e) => {
           setRating(e.target.value);
