32be3e8c2970a1a15095aed2946587e2a81cdd80
Megan Sullivan <meganesulli@gmail.com>
2022-01-25 17:26:17 -0800

    fix: add latestReviews resolver to reviews starter code

--- your-code-here/subgraph-reviews/resolvers.js
+++ your-code-here/subgraph-reviews/resolvers.js
@@ -1,9 +1,14 @@
 const resolvers = {
-  Mutation: {
-    submitReview(_, { review }, { dataSources }) {
-      const newReview = dataSources.reviewsAPI.submitReviewForLocation(review);
-      return { code: 200, success: true, message: 'success', review: newReview };
-    },
+  Query: {
+    latestReviews(_, __, {dataSources}) {
+      return dataSources.reviewsAPI.getLatestReviews();
+    }
   },
+  Mutation: {
+    submitReview(_, {review}, {dataSources}) {
+      const newReview = dataSources.reviewsAPI.submitReviewForLocation(review);
+      return {code: 200, success: true, message: 'success', review: newReview};
+    }
+  }
 };
 module.exports = resolvers;
