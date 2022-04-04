23c409c4bb7b1b8b11f5aecf6b63f3c6965d23a7
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2022-04-04 14:43:50 -0600

    fix(final-resolvers): set up final resolvers with updates to mutation

--- final/subgraph-reviews/resolvers.js
+++ final/subgraph-reviews/resolvers.js
@@ -18,9 +18,9 @@ const resolvers = {
     }
   },
   Mutation: {
-    submitReview(_, {review}, {dataSources}) {
-      const newReview = dataSources.reviewsAPI.submitReviewForLocation(review);
-      return {code: 200, success: true, message: 'success', review: newReview};
+    submitReview(_, {locationReview}, {dataSources}) {
+      const newReview = dataSources.reviewsAPI.submitReviewForLocation(locationReview);
+      return {code: 200, success: true, message: 'success', locationReview: newReview};
     }
   }
 };
