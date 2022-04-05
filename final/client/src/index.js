0afafb209a5b5dec9482d1eac7c48b3117a45ccc
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2022-04-05 14:59:43 -0600

    fix(setup): set up for final client build

--- final/client/src/index.js
+++ final/client/src/index.js
@@ -11,8 +11,13 @@ import {
 import theme from './theme.js';
 import {ChakraProvider} from '@chakra-ui/react';
 
+const gqlServerAddress =
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:4000'
+    : process.env.REACT_APP_GQL_SERVER;
+
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+  uri: gqlServerAddress
 });
 
 const client = new ApolloClient({
