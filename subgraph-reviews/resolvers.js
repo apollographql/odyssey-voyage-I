53eb2de3f115dd7073967ececcbe92473662e0e5
craigkehl <keh19003@byui.edu>
2022-11-22 15:31:50 -0700

    final

--- subgraph-reviews/resolvers.js
+++ subgraph-reviews/resolvers.js
@@ -1,15 +1,34 @@
 const resolvers = {
   Query: {
-    latestReviews: (_, __, {dataSources}) => {
+    latestReviews: (_, __, { dataSources }) => {
       return dataSources.reviewsAPI.getLatestReviews();
-    }
+    },
+  },
+  Location: {
+    overallRating: ({ id }, _, { dataSources }) => {
+      return dataSources.reviewsAPI.getOverallRatingForLocation(id);
+    },
+    reviewsForLocation: ({ id }, _, { dataSources }) => {
+      return dataSources.reviewsAPI.getReviewsForLocation(id);
+    },
+  },
+  Review: {
+    location: ({ locationId }) => {
+      return { id: locationId };
+    },
   },
   Mutation: {
-    submitReview: (_, {locationReview}, {dataSources}) => {
-      const newReview = dataSources.reviewsAPI.submitReviewForLocation(locationReview);
-      return {code: 200, success: true, message: 'success', locationReview: newReview};
-    }
-  }
+    submitReview: (_, { locationReview }, { dataSources }) => {
+      const newReview =
+        dataSources.reviewsAPI.submitReviewForLocation(locationReview);
+      return {
+        code: 200,
+        success: true,
+        message: 'success',
+        locationReview: newReview,
+      };
+    },
+  },
 };
 
 module.exports = resolvers;
